99d161be4a184608cdb9b75e31e6989d084fd335
Daniele Di Sarli <danieleds0@gmail.com>
2013-07-22 23:29:21 +0200

    Fix bugs

--- main.cpp
+++ main.cpp
@@ -29,11 +29,21 @@ void sigHandler(int sig)
 }
 
 void enableALS(bool enable) {
-    if(enable) {
-        system("echo '\\_SB.PCI0.LPCB.EC0.TALS 0x1' > sudo tee /proc/acpi/call");
-    } else {
-        system("echo '\\_SB.PCI0.LPCB.EC0.TALS 0x0' > sudo tee /proc/acpi/call");
+    int fd = open("/sys/bus/acpi/devices/ACPI0008:00/ali", O_RDONLY);
+    if(fd == -1) {
+        fprintf(stderr, "Error opening /proc/acpi/call");
     }
+
+    char *buf;
+    if(enable) {
+        buf = "\\_SB.PCI0.LPCB.EC0.TALS 0x1";
+    } else {
+        buf = "\\_SB.PCI0.LPCB.EC0.TALS 0x0";
+    }
+
+    write(fd, buf, strlen(buf) + 1);
+
+    close(fd);
 }
 
 void showNotification(bool enabled) {
@@ -44,27 +54,26 @@ void showNotification(bool enabled) {
     }
 }
 
-void setScreenBacklight(float percent) {
-    int xblvalue = (int)(percent * 100);
+void setScreenBacklight(int percent) {
     char cmd[100];
-    snprintf(cmd, 100, "xbacklight -set %d", xblvalue);
+    snprintf(cmd, 100, "xbacklight -set %d", percent);
     system(cmd);
 }
 
-void setKeyboardBacklight(float percent) {
+void setKeyboardBacklight(int percent) {
     int value = 0;
 
-    if(percent <= 0.25) value = 0;
-    else if(percent <= 0.50) value = 1;
-    else if(percent <= 0.75) value = 2;
-    else if(percent <= 1.00) value = 3;
+    if(percent <= 25) value = 0;
+    else if(percent <= 50) value = 1;
+    else if(percent <= 75) value = 2;
+    else if(percent <= 100) value = 3;
 
     char cmd[150];
     snprintf(cmd, 150, "echo %d | sudo tee /sys/class/leds/asus::kbd_backlight/brightness", value);
     system(cmd);
 }
 
-float getAmbientLightPercent() {
+int getAmbientLightPercent() {
     int fd = open("/sys/bus/acpi/devices/ACPI0008:00/ali", O_RDONLY);
     if(fd == -1) {
         fprintf(stderr, "Error opening ali");
@@ -72,30 +81,31 @@ float getAmbientLightPercent() {
     }
     char strals[100];
     int count = read(fd, strals, 100);
-    printf("%s", strals);
     strals[count] = '\0';
     close(fd);
 
     // 0x32 (min illuminance), 0xC8, 0x190, 0x258, 0x320 (max illuminance).
     int als = atoi(strals);
+    printf("\"%s\"\n", strals);
+    printf("%d\n", als);
 
-    int percent = 0;
+    float percent = 0;
 
     switch(als) {
     case 0x32:
-        percent = 0.10;
+        percent = 10;
         break;
     case 0xC8:
-        percent = 0.25;
+        percent = 25;
         break;
     case 0x190:
-        percent = 0.50;
+        percent = 50;
         break;
     case 0x258:
-        percent = 0.75;
+        percent = 75;
         break;
     case 0x320:
-        percent = 1.00;
+        percent = 100;
         break;
     }
 
@@ -110,7 +120,8 @@ int main()
     sa.sa_flags = 0;
     sigaction(SIGUSR1, &sa, NULL);
 
-    system("notify-send 'Luminosità automatica'");
+    enableALS(true);
+    showNotification(true);
 
     while(1) {
 
@@ -132,25 +143,25 @@ int main()
         }
 
         float als = getAmbientLightPercent();
-
-        if(als <= 0.10) {
-            setScreenBacklight(0.40);
-            setKeyboardBacklight(1.0);
-        } else if(als <= 0.25) {
-            setScreenBacklight(0.60);
+        printf("%f\%\n", als);
+        if(als <= 10) {
+            setScreenBacklight(40);
+            setKeyboardBacklight(100);
+        } else if(als <= 25) {
+            setScreenBacklight(60);
             setKeyboardBacklight(0);
-        } else if(als <= 0.50) {
-            setScreenBacklight(0.75);
+        } else if(als <= 50) {
+            setScreenBacklight(75);
             setKeyboardBacklight(0);
-        } else if(als <= 0.75) {
-            setScreenBacklight(0.90);
+        } else if(als <= 75) {
+            setScreenBacklight(90);
             setKeyboardBacklight(0);
-        } else if(als <= 1.00) {
-            setScreenBacklight(1.00);
+        } else if(als <= 100) {
+            setScreenBacklight(100);
             setKeyboardBacklight(0);
         }
 
-        sleep(5);
+        sleep(3);
     }
 
     return 0;
