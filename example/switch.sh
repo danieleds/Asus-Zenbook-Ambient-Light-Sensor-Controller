d3ac59afbae9128d99da286652f12f187175f5e4
Daniele Di Sarli <danieleds0@gmail.com>
2014-01-21 22:16:09 +0100

    Fix parameter

--- example/switch.sh
+++ example/switch.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 STATUS=$("$DIR"/../service/als-controller -s)
-if [ "$STATUS" = "1" ]; then
-  "$DIR"/../service/als-controller -d
-  notify-send -c "device" -i "$DIR/"'images/active.svg' 'Ambient Light Sensor' 'Enabled'
-elif [ "$STATUS" = "0" ]; then
+if [ "$STATUS" = "0" ]; then
   "$DIR"/../service/als-controller -e
+  notify-send -c "device" -i "$DIR/"'images/active.svg' 'Ambient Light Sensor' 'Enabled'
+elif [ "$STATUS" = "1" ]; then
+  "$DIR"/../service/als-controller -d
   notify-send -c "device" -i "$DIR/"'images/inactive.svg' 'Ambient Light Sensor' 'Disabled'
 else
   echo "Error: $STATUS"
