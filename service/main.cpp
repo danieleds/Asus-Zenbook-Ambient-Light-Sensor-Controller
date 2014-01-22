74403eeca3bad469dba8a36952c524a82aaa6714
Daniele Di Sarli <danieleds0@gmail.com>
2014-01-22 12:51:58 +0100

    Daemonized

--- service/main.cpp
+++ service/main.cpp
@@ -22,11 +22,15 @@
 #include <signal.h>
 #include <stdio.h>
 #include <pthread.h>
+#include <syslog.h>
 #include "comsock.h"
 #include "client.h"
 
 using namespace std;
 
+void logServerExit(int __status, int __pri, const char *fmt...);
+void startDaemon();
+void enableALS(bool enable);
 void *IPCHandler(void *arg);
 void *clientHandler(void *arg);
 
@@ -40,6 +44,9 @@ char* C_SOCKET_PATH = (char*)SOCKET_PATH.c_str();
 pthread_mutex_t mtx = PTHREAD_MUTEX_INITIALIZER;
 pthread_cond_t start = PTHREAD_COND_INITIALIZER;
 
+/** Signal mask */
+static sigset_t g_sigset;
+
 /*void sigHandler(int sig)
 {
     if(sig == SIGUSR1) {
@@ -47,10 +54,34 @@ pthread_cond_t start = PTHREAD_COND_INITIALIZER;
     }
 }*/
 
+void *sigManager(void *arg) {
+  int signum;
+
+  while(1) {
+    sigwait(&g_sigset, &signum);
+
+    if(signum == SIGINT || signum == SIGTERM) {
+        logServerExit(EXIT_SUCCESS, LOG_INFO, "Terminated.");
+    }
+  }
+
+  return NULL;
+}
+
+void logServerExit(int __status, int __pri, const char *fmt...) {
+    closeServerChannel(C_SOCKET_PATH, g_socket);
+    enableALS(false);
+    syslog(__pri, fmt);
+    if(__status != EXIT_SUCCESS)
+        syslog(LOG_INFO, "Terminated.");
+    closelog();
+    exit(__status);
+}
+
 void enableALS(bool enable) {
-    int fd = open("/sys/bus/acpi/devices/ACPI0008:00/ali", O_RDONLY);
+    int fd = open("/proc/acpi/call", O_RDONLY);
     if(fd == -1) {
-        fprintf(stderr, "Error opening /proc/acpi/call");
+        logServerExit(EXIT_FAILURE, LOG_CRIT, "Error opening /proc/acpi/call");
     }
 
     string buf;
@@ -64,7 +95,10 @@ void enableALS(bool enable) {
 
     close(fd);
 
-    printf("ALS enabled = %d\n", enable);
+    if (enable)
+        syslog(LOG_INFO, "ALS enabled");
+    else
+        syslog(LOG_INFO, "ALS disabled");
 }
 
 void setScreenBacklight(int percent) {
@@ -89,8 +123,7 @@ void setKeyboardBacklight(int percent) {
 int getAmbientLightPercent() {
     int fd = open("/sys/bus/acpi/devices/ACPI0008:00/ali", O_RDONLY);
     if(fd == -1) {
-        fprintf(stderr, "Error opening ali");
-        return -1;
+        logServerExit(EXIT_FAILURE, LOG_CRIT, "Error opening /sys/bus/acpi/devices/ACPI0008:00/ali");
     }
     char strals[100];
     int count = read(fd, strals, 100);
@@ -100,7 +133,7 @@ int getAmbientLightPercent() {
     // 0x32 (min illuminance), 0xC8, 0x190, 0x258, 0x320 (max illuminance).
     int als = atoi(strals);
     //printf("\"%s\"\n", strals);
-    printf("Illuminance detected: %d\n", als);
+    //printf("Illuminance detected: %d\n", als);
 
     float percent = 0;
 
@@ -139,10 +172,71 @@ int main(int argc, char *argv[])
         exit(EXIT_SUCCESS);
     }
 
+
+    pid_t pid;
+
+    /* Fork off the parent process */
+    pid = fork();
+    if (pid < 0) {
+        exit(EXIT_FAILURE);
+    }
+    /* If we got a good PID, then
+       we can exit the parent process. */
+    if (pid > 0) {
+        exit(EXIT_SUCCESS);
+    }
+
+    /* Change the file mode mask */
+    umask(0);
+
+    /* Create a new SID for the child process */
+    if (setsid() < 0)
+        exit(EXIT_FAILURE);
+
+    /* Change the current working directory */
+    if ((chdir("/")) < 0) {
+        /* Log any failure here */
+        exit(EXIT_FAILURE);
+    }
+
+    /* Close out the standard file descriptors */
+    close(STDIN_FILENO);
+    close(STDOUT_FILENO);
+    close(STDERR_FILENO);
+
+    /* Open the log file */
+    openlog("als-controller", LOG_PID, LOG_DAEMON);
+
+    startDaemon();
+
+    return 0;
+}
+
+void startDaemon()
+{
+    syslog(LOG_NOTICE, "Started.");
+
+    /* Maschera i signal handler. La maschera viene ereditata dai thread successivi
+     * (quindi questi segnali verranno bloccati in tutti i thread).
+     * In particolare, il thread "sigthread" si occuperà di gestire tutti i segnali
+     * che qui stiamo bloccando. */
+    sigemptyset(&g_sigset);
+    sigaddset(&g_sigset, SIGINT);
+    sigaddset(&g_sigset, SIGTERM);
+    if(pthread_sigmask(SIG_SETMASK, &g_sigset, NULL) != 0) {
+        logServerExit(EXIT_FAILURE, LOG_CRIT, "Sigmask error.");
+    }
+
+    pthread_t sigthread;
+    if(pthread_create(&sigthread, NULL, sigManager, NULL) != 0) {
+        logServerExit(EXIT_FAILURE, LOG_CRIT, "Creating thread.");
+    }
+
+
     pthread_t thread_id;
     int err = pthread_create(&thread_id, NULL, IPCHandler, NULL);
     if(err != 0) {
-        perror("Creating thread");
+        syslog(LOG_CRIT, "Cannot create thread");
         exit(EXIT_FAILURE);
     }
 
@@ -155,7 +249,7 @@ int main(int argc, char *argv[])
         pthread_mutex_unlock(&mtx);
 
         float als = getAmbientLightPercent();
-        printf("Illuminance percent: %f\n", als);
+        //printf("Illuminance percent: %f\n", als);
         if(als <= 10) {
             setScreenBacklight(40);
             setKeyboardBacklight(100);
@@ -176,7 +270,7 @@ int main(int argc, char *argv[])
         sleep(3);
     }
 
-    return 0;
+    logServerExit(EXIT_SUCCESS, LOG_NOTICE, "Terminated.");
 }
 
 void *IPCHandler(void *arg)
@@ -184,8 +278,7 @@ void *IPCHandler(void *arg)
     unlink(C_SOCKET_PATH);
     g_socket = createServerChannel(C_SOCKET_PATH);
     if(g_socket == -1) {
-      perror("Creating socket");
-      exit(EXIT_FAILURE);
+      logServerExit(EXIT_FAILURE, LOG_CRIT, "Error creating socket");
     }
 
     // Permessi 777 sulla socket
@@ -198,13 +291,12 @@ void *IPCHandler(void *arg)
     {
         int client = acceptConnection(g_socket);
         if(client == -1) {
-            perror("Incoming connection");
+            syslog(LOG_ERR, "Error accepting client connection.");
         } else {
             pthread_t thread_id;
             int err = pthread_create(&thread_id, NULL, clientHandler, (void *)(size_t)client);
             if(err != 0) {
-                perror("Creating thread");
-                exit(EXIT_FAILURE);
+                logServerExit(EXIT_FAILURE, LOG_CRIT, "Error creating client thread.");
             }
         }
     }
@@ -215,8 +307,10 @@ void *clientHandler(void *arg)
     int client = (int)(size_t)arg;
     message_t msg;
 
-    if(receiveMessage(client, &msg) == -1)
+    if(receiveMessage(client, &msg) == -1) {
+        syslog(LOG_ERR, "Error receiving message from client.");
         return NULL;
+    }
 
     if(msg.type == MSG_ENABLE) {
         enableALS(true);
@@ -244,7 +338,8 @@ void *clientHandler(void *arg)
 
         sent = sendMessage(client, &msg);
         if(sent == -1) {
-          perror("Error sending reply.");
+            syslog(LOG_ERR, "Error sending reply to client.");
+            return NULL;
         }
     }
 
